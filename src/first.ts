0eddbd6abc9277e297cb0fc0ec65b9f932621cac
jakub-wasik <jakub.wasik@schibsted.com>
2023-11-04 15:06:40 +0100

    Second test

--- src/first.ts
+++
@@ -1,1 +0,0 @@
-export const dupa = (a: number, b: number) => a+b;
--- src/second/game.test.ts
+++ src/second/game.test.ts
@@ -1,6 +1,8 @@
 type SurviveFn = (neighbours: number) => boolean
 
-const survive: SurviveFn = () => true;
+const survive: SurviveFn = (neighbours) => {
+  return neighbours >= 2;
+};
 
 describe("game", () => {
   test("Cell should survive for 2 neighbours", () => {
@@ -12,5 +14,16 @@ describe("game", () => {
 
     // Then
     expect(result).toBe(true);
+  });
+
+  test("Cell should die for less than 2 neighbours", () => {
+    // Given
+    const neighbours = 1;
+
+    // When
+    const result = survive(neighbours);
+
+    // Then
+    expect(result).toBe(false);
   });
 });
