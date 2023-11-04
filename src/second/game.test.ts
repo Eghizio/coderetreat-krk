0a756877f467ad3fdabda1dad1cdbb8bc4e21cdf
jakub-wasik <jakub.wasik@schibsted.com>
2023-11-04 15:11:59 +0100

    TCR survive

--- src/second/game.test.ts
+++ src/second/game.test.ts
@@ -4,48 +4,51 @@ const survive: SurviveFn = (neighbours) => {
   return neighbours == 2 || neighbours == 3;
 };
 
+// TCR - Test and Commit or Revert!
 describe("game", () => {
-  test("Cell should survive for 2 neighbours", () => {
-    // Given
-    const neighbours = 2;
+  describe("survive", () => {
+    test("Cell should survive for 2 neighbours", () => {
+      // Given
+      const neighbours = 2;
+  
+      // When
+      const result = survive(neighbours);
+  
+      // Then
+      expect(result).toBe(true);
+    });
 
-    // When
-    const result = survive(neighbours);
-
-    // Then
-    expect(result).toBe(true);
-  });
-
-  test("Cell should survive for 3 neighbours", () => {
-    // Given
-    const neighbours = 3;
-
-    // When
-    const result = survive(neighbours);
-
-    // Then
-    expect(result).toBe(true);
-  });
-
-  test("Cell should die for less than 2 neighbours", () => {
-    // Given
-    const neighbours = 1;
-
-    // When
-    const result = survive(neighbours);
-
-    // Then
-    expect(result).toBe(false);
-  });
-
-  test("Cell should die for more than 3 neighbours", () => {
-    // Given
-    const neighbours = 4;
-
-    // When
-    const result = survive(neighbours);
-
-    // Then
-    expect(result).toBe(false);
+    test("Cell should survive for 3 neighbours", () => {
+      // Given
+      const neighbours = 3;
+  
+      // When
+      const result = survive(neighbours);
+  
+      // Then
+      expect(result).toBe(true);
+    });
+  
+    test("Cell should die for less than 2 neighbours", () => {
+      // Given
+      const neighbours = 1;
+  
+      // When
+      const result = survive(neighbours);
+  
+      // Then
+      expect(result).toBe(false);
+    });
+  
+    test("Cell should die for more than 3 neighbours", () => {
+      // Given
+      const neighbours = 4;
+  
+      // When
+      const result = survive(neighbours);
+  
+      // Then
+      expect(result).toBe(false);
+    });
   });
 });
